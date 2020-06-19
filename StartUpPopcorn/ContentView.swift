c62885a5dad5f4ddefda8f472e949942d52a3415
Naoki Yoshida <1711140006e@ed.fuk.kindai.ac.jp>
2020-06-19 14:48:58 +0900

    Initial Commit

---
+++ StartUpPopcorn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StartUpPopcorn
+//
+//  Created by Naoki Yoshida on 2020/06/19.
+//  Copyright © 2020 Naoki Yoshida. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
